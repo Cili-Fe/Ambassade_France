bee31ffb25c641ccb20611fc506dc87f342e4824
Kabor-f <Kaborefelicite04@gmail.com>
2020-12-18 18:55:29 +0100

    ambassade

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const $ = (el) => {
+        return document.querySelector(el);
+}
+let diapo_img = $('.diapo_img');
+let date = $('#date');
+let diapo_heading = $('#title');
+let diapo_text = $('.diapo_text');
